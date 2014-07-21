4dc8f77168f8df45151d3cd79cf4f89901fb6363
Cristiano Veloz <ctoveloz@gmail.com>
2014-07-20 21:37:50 -0300

    app.js update

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 // Variables
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+app.use(app.router);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Port
 app.listen(3200);
 
-// Run NodeJS app
+// Routes
 app.get('/', function(req, res) {
 
 	res.end('Servidor ON!!!');
 });
+
+// Route Login
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure'
+  })
+);
+ 
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+ 
+app.get('/loginSuccess', function(req, res, next) {
+  res.send('Successfully authenticated');
+});
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+ 
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  process.nextTick(function() {
+
+  });
+}));
